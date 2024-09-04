6cf1303d46748816453cbf8e90053099daa78323
Akshathello <akshatkhero@gmail.com>
2024-09-04 15:32:53 +0530

    Vision Part

--- src/pages/AboutUs/MissionVision.jsx
+++ src/pages/AboutUs/MissionVision.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Mission from "../../../public/assets/Mission.webp"
+import Vision from "../../../public/assets/Vision.webp"
 
 const MissionVision = () => {
   return (
@@ -30,7 +31,33 @@ const MissionVision = () => {
       
       </div>
 
-    </section>
+      <div className =" max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-center gap-10 border-t-2 border-[#C49E5F] mt-10">
+
+      <div className = " mt-8">
+
+
+      <img src={Vision} alt="" className= "h-auto w-[100%] object-cover"/>
+         </div>
+
+          <div className= "md:w-2/3 p-10">
+          <div className= "text-[#C49E5F] lg:text-7xl md:text-5xl cinzel-font text-end mb-10">
+           Our Vision
+            </div>
+           <div className= " text-black text-xl mt-0 tracking-normal font-medium text-end">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique animi explicabo veniam? Blanditiis perferendis, nesciunt cupiditate alias praesentium id iure sequi, consequuntur placeat qui nostrum quae quidem voluptatum beatae aspernatur!        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique animi explicabo veniam? Blanditiis perferendis, nesciunt cupiditate alias praesentium id iure sequi, consequuntur placeat qui nostrum quae quidem voluptatum beatae aspernatur!
+
+
+           </div>
+              </div>
+
+
+
+
+
+
+               </div>
+
+                </section>
 
 
 
