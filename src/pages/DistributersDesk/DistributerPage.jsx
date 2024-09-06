bc1caba4ba9ae83fcfb64274dc69ce6e27edf1aa
Supriyo Saha <mm.saha1982@gmail.com>
2024-09-07 03:04:25 +0530

    fix

--- src/pages/DistributersDesk/DistributerPage.jsx
+++ src/pages/DistributersDesk/DistributerPage.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
-import Navbar from '../../components/Navbar'
-import Footer from '../../components/Footer'
-import DistributerBenefit from './DistributerBenefit'
-import DistributorHome from './DistributorHome'
-import DistributerFeatures from './DistributerFeatures'
-import FaqSection from './FaqSection'
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import DistributerBenefit from "./DistributerBenefit";
+import DistributorHome from "./DistributorHome";
+import DistributerFeatures from "./DistributerFeatures";
+import FaqSection from "./FaqSection";
 
 function DistributerPage() {
   return (
     <main className="bg-[#FAF5EF]">
-        <Navbar/>
-        <DistributorHome/>
-        <DistributerBenefit/>
-        <DistributerFeatures/>
-        <FaqSection/>
-        <Footer/>
+      <Navbar />
+      <section className="lg:my-12">
+        <DistributorHome />
+      </section>
+      <section>
+        <DistributerBenefit />
+      </section>
+      <section>
+        <DistributerFeatures />
+      </section>
+      <section>
+        <FaqSection />
+      </section>
 
+      <Footer />
     </main>
-  )
+  );
 }
 
-export default DistributerPage
+export default DistributerPage;
