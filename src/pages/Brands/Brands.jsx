3e4644662eae541d108a98fb6bf0a869e1033baf
Supriyo Saha <mm.saha1982@gmail.com>
2024-09-10 11:25:46 +0530

    brands start

---
+++ src/pages/Brands/Brands.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Slide } from 'react-awesome-reveal'
+
+const Brands = () => {
+  return (
+    <section className='py-10'>
+      <div className="lg:text-8xl text-5xl text-center text-[#C49E5F] lg:tracking-wider cinzel-font uppercase lg:py-8 py-4">
+        <Slide direction='down'>
+         Explore our Brands
+        </Slide>
+      </div>
+
+      <div className='flex justify-center gap-10 lg:flex-row flex-col'>
+
+      </div>
+    </section>
+  )
+}
+
+export default Brands
--- src/pages/Brands/BrandsPage.jsx
+++ src/pages/Brands/BrandsPage.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import BrandsHero from "./BrandsHero";
+import Brands from "./Brands";
 
 function BrandsPage() {
   return (
-    <div>BrandsPage</div>
-  )
+    <main className="bg-[#FAF5EF]">
+      <Navbar />
+      <section>
+        <BrandsHero />
+      </section>
+
+      <section>
+        <Brands />
+      </section>
+      <Footer />
+    </main>
+  );
 }
 
-export default BrandsPage
+export default BrandsPage;
